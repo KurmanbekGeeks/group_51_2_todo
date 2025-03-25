4344246f271d8eb729ed6d8cfb19f99bf5721076
kurmanbek <kmomunkulov@crm.kg>
2025-03-25 20:39:47 +0600

    Lesson 6.2

--- db/main_db.py
+++ db/main_db.py
@@ -11,10 +11,18 @@ def init_db():
     conn.close()
 
 
-def get_tasks():
+def get_tasks(filter_type="all"):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute(queries.SELECT_TASKS)
+    # cursor.execute(queries.SELECT_TASKS)
+
+    if filter_type == 'completed':
+        cursor.execute(queries.SELECT_completed)
+    elif filter_type == "incomplete":
+        cursor.execute(queries.SELECT_incomplete)
+    else:
+        cursor.execute(queries.SELECT_TASKS)
+
     tasks = cursor.fetchall()
     conn.close()
     return tasks
@@ -30,13 +38,26 @@ def add_task_db(task):
     return task_id
 
 
-def update_task_db(task_id, new_task):
+# def update_task_db(task_id, new_task):
+#     conn = sqlite3.connect(DB_PATH)
+#     cursor = conn.cursor()
+#     cursor.execute(queries.UPDATE_TASK, (new_task, task_id))
+#     conn.commit()
+#     conn.close()
+
+
+def update_task_db(task_id, new_task=None, completed=None):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute(queries.UPDATE_TASK, (new_task, task_id))
+    # cursor.execute(queries.UPDATE_TASK, (new_task, task_id))
+
+    if new_task is not None:
+        cursor.execute("UPDATE tasks SET task = ? WHERE id = ?", (new_task, task_id))
+    if completed is not None:
+        cursor.execute("UPDATE tasks SET completed = ? WHERE id = ?", (completed, task_id))
+
     conn.commit()
     conn.close()
-
 
 def delete_task_db(task_id):
     conn = sqlite3.connect(DB_PATH)
--- db/queries.py
+++ db/queries.py
@@ -1,11 +1,12 @@
 CREATE_TABLE_TASKS = """
     CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        task TEXT NOT NULL
+        task TEXT NOT NULL,
+        completed INTEGER DEFAULT 0
         )
 """
 
-SELECT_TASKS = "SELECT id, task FROM tasks"
+SELECT_TASKS = "SELECT id, task, completed FROM tasks"
 
 INSERT_TASK = "INSERT INTO tasks (task) VALUES (?)"
 
@@ -13,3 +14,7 @@ UPDATE_TASK = "UPDATE tasks SET task = ? WHERE id = ?"
 
 DELETE_TASK = "DELETE FROM tasks WHERE id = ?"
 
+
+SELECT_completed = 'SELECT id, task, completed FROM tasks WHERE completed = 1'
+
+SELECT_incomplete = 'SELECT id, task, completed FROM tasks WHERE completed = 0'
